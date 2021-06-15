6df0ebee29cd6d78a71bbbaf28f27e6f9b2339d4
Lisandro Volpe <lichivolpe@gmail.com>
2021-06-14 23:51:32 -0300

    se agrego components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <nav>
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="l">Nuevo</a></li>
+				<li><a href="">Productos</a></li>
+				<li><a href="">Contacto</a></li>
+			</ul>
+		</nav>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+    return <>
+        <nav style={{with: '100%', height: '60px', background: 'white'}}>
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="l">Nuevo</a></li>
+				<li><a href="">Productos</a></li>
+				<li><a href="">Contacto</a></li>
+			</ul>
+		</nav>
+     </>;
+}
