907c630762e773750181a08c485914f813479190
Lisandro Volpe <lichivolpe@gmail.com>
2021-06-16 20:12:33 -0300

    correccion entrega de la clase 3

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav>
-			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="l">Nuevo</a></li>
-				<li><a href="">Productos</a></li>
-				<li><a href="">Contacto</a></li>
-			</ul>
-		</nav>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavBar/>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
+import react from 'react'
+
 function NavBar() {
     return <>
-        <nav style={{with: '100%', height: '60px', background: 'white'}}>
-			<ul>
+        <nav>
+			<ul style={{listStyle: 'none'}}>
 				<li><a href="">Home</a></li>
-				<li><a href="l">Nuevo</a></li>
+				<li><a href="">Nuevo</a></li>
 				<li><a href="">Productos</a></li>
 				<li><a href="">Contacto</a></li>
 			</ul>
 		</nav>
      </>;
 }
+
+
+export default NavBar;
